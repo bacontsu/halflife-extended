f998aa2d1913d6c4d180a693aa3e460dc1c5238f
tear-vyps <77048123+tear-vyps@users.noreply.github.com>
2021-12-20 16:52:29 +0200

    Add files via upload

---
+++ source-code/pm_shared/pm_debug.h
@@ -0,0 +1,24 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+#ifndef PM_DEBUG_H
+#define PM_DEBUG_H
+#pragma once
+
+void PM_ViewEntity();
+void PM_DrawBBox(Vector mins, Vector maxs, Vector origin, int pcolor, float life);
+void PM_ParticleLine(Vector start, Vector end, int pcolor, float life, float vert);
+void PM_ShowClipBox();
+
+#endif // PMOVEDBG_H
