daa930079d390941ed7cdf5fae0933c1ce80d272
tear-vyps <77048123+tear-vyps@users.noreply.github.com>
2021-12-20 16:36:45 +0200

    Add files via upload

---
+++ source-code/cl_dlls/hl/hl_events.cpp
@@ -0,0 +1,70 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+#include "../hud.h"
+#include "../cl_util.h"
+#include "event_api.h"
+#include "pmtrace.h"
+#include "ev_hldm.h"
+
+/*
+======================
+Game_HookEvents
+
+Associate script file name with callback functions.  Callback's must be extern "C" so
+ the engine doesn't get confused about name mangling stuff.  Note that the format is
+ always the same.  Of course, a clever mod team could actually embed parameters, behavior
+ into the actual .sc files and create a .sc file parser and hook their functionality through
+ that.. i.e., a scripting system.
+
+That was what we were going to do, but we ran out of time...oh well.
+======================
+*/
+void Game_HookEvents()
+{
+	gEngfuncs.pfnHookEvent( "events/glock1.sc",					EV_FireGlock1 );
+	gEngfuncs.pfnHookEvent( "events/glock2.sc",					EV_FireGlock2 );
+	gEngfuncs.pfnHookEvent( "events/shotgun1.sc",				EV_FireShotGunSingle );
+	gEngfuncs.pfnHookEvent( "events/shotgun2.sc",				EV_FireShotGunDouble );
+	gEngfuncs.pfnHookEvent( "events/mp5.sc",					EV_FireMP5 );
+	gEngfuncs.pfnHookEvent( "events/mp52.sc",					EV_FireMP52 );
+	gEngfuncs.pfnHookEvent( "events/python.sc",					EV_FirePython );
+	gEngfuncs.pfnHookEvent( "events/gauss.sc",					EV_FireGauss );
+	gEngfuncs.pfnHookEvent( "events/gaussspin.sc",				EV_SpinGauss );
+	gEngfuncs.pfnHookEvent( "events/train.sc",					EV_TrainPitchAdjust );
+	gEngfuncs.pfnHookEvent( "events/crowbar.sc",				EV_Crowbar );
+	gEngfuncs.pfnHookEvent( "events/crossbow1.sc",				EV_FireCrossbow );
+	gEngfuncs.pfnHookEvent( "events/crossbow2.sc",				EV_FireCrossbow2 );
+	gEngfuncs.pfnHookEvent( "events/rpg.sc",					EV_FireRpg );
+	gEngfuncs.pfnHookEvent( "events/egon_fire.sc",				EV_EgonFire );
+	gEngfuncs.pfnHookEvent( "events/egon_stop.sc",				EV_EgonStop );
+	gEngfuncs.pfnHookEvent( "events/firehornet.sc",				EV_HornetGunFire );
+	gEngfuncs.pfnHookEvent( "events/tripfire.sc",				EV_TripmineFire );
+	gEngfuncs.pfnHookEvent( "events/snarkfire.sc",				EV_SnarkFire );
+	gEngfuncs.pfnHookEvent( "events/eagle.sc",					EV_FireEagle );
+	gEngfuncs.pfnHookEvent( "events/pipewrench.sc",				EV_Pipewrench );
+	gEngfuncs.pfnHookEvent( "events/m249.sc",					EV_FireM249 );
+	gEngfuncs.pfnHookEvent( "events/displacer.sc",				EV_FireDisplacer );
+	gEngfuncs.pfnHookEvent( "events/shock.sc",					EV_FireShockRifle );
+	gEngfuncs.pfnHookEvent( "events/spore.sc",					EV_FireSpore );
+	gEngfuncs.pfnHookEvent( "events/sniper.sc",					EV_SniperRifle );
+	gEngfuncs.pfnHookEvent( "events/knife.sc",					EV_Knife );
+	gEngfuncs.pfnHookEvent("events/penguinfire.sc", EV_PenguinFire);
+	gEngfuncs.pfnHookEvent("events/chumtoadfire.sc", EV_FireChumtoad);
+	//gEngfuncs.pfnHookEvent("events/ber1.sc", EV_FireBeretta1);
+	//gEngfuncs.pfnHookEvent("events/ber2.sc", EV_FireBeretta2);
+	//gEngfuncs.pfnHookEvent("events/baton.sc", EV_Baton);
+	gEngfuncs.pfnHookEvent("events/ar1.sc", EV_ar1);
+	gEngfuncs.pfnHookEvent("events/m79.sc", EV_GLFire);
+}
