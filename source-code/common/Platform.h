19e4ff35acab4352223857a1a0ee7201af2ceda6
tear-vyps <77048123+tear-vyps@users.noreply.github.com>
2021-12-20 16:39:25 +0200

    Add files via upload

---
+++ source-code/common/Platform.h
@@ -0,0 +1,112 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+/**
+*	@file
+*
+*	Platform abstractions, common header includes, workarounds for compiler warnings
+*/
+
+// Allow "DEBUG" in addition to default "_DEBUG"
+#ifdef _DEBUG
+#define DEBUG 1
+#endif
+
+// Silence certain warnings
+#pragma warning(disable : 4244)		// int or float down-conversion
+#pragma warning(disable : 4305)		// int or float data truncation
+#pragma warning(disable : 4201)		// nameless struct/union
+#pragma warning(disable : 4514)		// unreferenced inline function removed
+#pragma warning(disable : 4100)		// unreferenced formal parameter
+
+#include "archtypes.h"     // DAL
+
+// Misc C-runtime library headers
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using byte = unsigned char;
+using word = unsigned short;
+using func_t = unsigned int;
+using string_t = unsigned int;
+using qboolean = int;
+
+#define ARRAYSIZE(p)		(sizeof(p)/sizeof(p[0]))
+
+// Prevent tons of unused windows definitions
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOWINRES
+#define NOSERVICE
+#define NOMCX
+#define NOIME
+#define NOMINMAX
+#include "winsani_in.h"
+#include <Windows.h>
+#include "winsani_out.h"
+#include <malloc.h>
+
+//Avoid the ISO conformant warning
+#define stricmp _stricmp
+#define strnicmp _strnicmp
+#define itoa _itoa
+#define strupr _strupr
+#define strdup _strdup
+
+#define DLLEXPORT __declspec( dllexport )
+
+#define stackalloc(size) _alloca(size)
+
+//Note: an implementation of stackfree must safely ignore null pointers
+#define stackfree(address)
+
+#else // _WIN32
+#define FALSE 0
+#define TRUE (!FALSE)
+typedef uint32 ULONG;
+typedef unsigned char BYTE;
+typedef int BOOL;
+#define MAX_PATH PATH_MAX
+#include <limits.h>
+#include <stdarg.h>
+#include <alloca.h>
+#define _vsnprintf(a,b,c,d) vsnprintf(a,b,c,d)
+
+#define stricmp strcasecmp
+#define _strnicmp strncasecmp
+#define strnicmp strncasecmp
+#define _snprintf snprintf
+#define _alloca alloca
+
+#define DLLEXPORT __attribute__ ( ( visibility( "default" ) ) )
+
+#define stackalloc(size) alloca(size)
+
+//Note: an implementation of stackfree must safely ignore null pointers
+#define stackfree(address)
+
+#endif //_WIN32
+
+#define V_min(a,b)  (((a) < (b)) ? (a) : (b))
+#define V_max(a,b)  (((a) > (b)) ? (a) : (b))
+
+#define clamp( val, min, max ) ( ((val) > (max)) ? (max) : ( ((val) < (min)) ? (min) : (val) ) )
+
+#endif //PLATFORM_H
---
+++ source-code/common/parsemsg.h
@@ -0,0 +1,66 @@
+/***
+*
+*	Copyright (c) 1999, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+//
+//  parsemsg.h
+//	MDC - copying from cstrike\cl_dll so career-mode stuff can catch messages
+//  in this dll. (and C++ifying it)
+//
+
+#ifndef PARSEMSG_H
+#define PARSEMSG_H
+
+#define ASSERT( x )
+//--------------------------------------------------------------------------------------------------------------
+void BEGIN_READ( void *buf, int size );
+int READ_CHAR();
+int READ_BYTE();
+int READ_SHORT();
+int READ_WORD();
+int READ_LONG();
+float READ_FLOAT();
+char* READ_STRING();
+float READ_COORD();
+float READ_ANGLE();
+float READ_HIRESANGLE();
+int READ_OK();
+
+//--------------------------------------------------------------------------------------------------------------
+class BufferWriter
+{
+public:
+	BufferWriter();
+	BufferWriter( unsigned char *buffer, int bufferLen );
+	void Init( unsigned char *buffer, int bufferLen );
+
+	void WriteByte( unsigned char data );
+	void WriteLong( int data );
+	void WriteString( const char *str );
+
+	bool HasOverflowed();
+	int GetSpaceUsed();
+
+protected:
+	unsigned char *m_buffer;
+	int m_remaining;
+	bool m_overflow;
+	int m_overallLength;
+};
+
+//--------------------------------------------------------------------------------------------------------------
+
+#endif // PARSEMSG_H
+
+
+
